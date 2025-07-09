772101b734250b34d8dfc72a47dd606d772cfbc2
htphong23 <148854970+htphong23@users.noreply.github.com>
2025-07-10 03:44:35 +0700

    initial commit with Jenkinsfile

---
+++ src/main/java/com/example/foodiehub/FoodiehubApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodiehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/foodiehub/FoodiehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodiehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
